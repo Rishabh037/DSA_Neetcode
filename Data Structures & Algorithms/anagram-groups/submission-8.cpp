61d9277072f9ffb4e7e027cec24fbe2ff3b2635c
Rishabh037 <162472774+Rishabh037@users.noreply.github.com>
2026-06-18 10:55:55 +0530

    Add: anagram-groups - submission-8

---
+++ Data Structures & Algorithms/anagram-groups/submission-8.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    vector<vector<string>> groupAnagrams(vector<string>& strs) {
+       unordered_map <string,vector<string>> res;
+       for(const auto &s : strs){
+        string sorts = s;
+        sort(sorts.begin(),sorts.end());
+        res[sorts].push_back(s);
+       }
+
+       vector<vector<string>> result;
+       for(auto & s : res){
+        result.push_back(s.second);
+       }
+
+       return result;
+
+    }
+};
